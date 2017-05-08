53a32c2dc5509b0f42a58a4de937dfe358e7f96b
luisazambuja <luisazambujafilho@gmail.com>
2017-05-08 12:21:30 -0300

    final hoje

--- Aula01/app/src/main/java/app/com/aula01/ExemploAula01.java
+++ Aula01/app/src/main/java/app/com/aula01/ExemploAula01.java
@@ -2,6 +2,9 @@ package app.com.aula01;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ExemploAula01 extends Activity {
 
@@ -9,5 +12,15 @@ public class ExemploAula01 extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exemplo_aula01);
+
+        final TextView textView = (TextView) findViewById(R.id.textView3);
+
+        Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                textView.setText("FUI DIBRADO");
+            }
+        });
     }
 }
