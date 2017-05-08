2988d53364363981f08df9e884412ee38500fd7b
luisazambuja <luisazambujafilho@gmail.com>
2017-05-08 11:59:44 -0300

    aula 01

---
+++ Aula01/app/src/main/java/app/com/aula01/ExemploAula01.java
@@ -0,0 +1,13 @@
+package app.com.aula01;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ExemploAula01 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exemplo_aula01);
+    }
+}
